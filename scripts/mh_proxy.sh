01eb496d2f4b411d85270d56c7b50d78c4c674a3
minerhome <minerhome.org@gmail.com>
2023-04-21 19:28:16 +0800

    Update mh_proxy.sh

--- scripts/mh_proxy.sh
+++ scripts/mh_proxy.sh
@@ -1,14 +1,11 @@
 #!/bin/bash
 
 
-if ps aux | grep 'mh_proxy'; then
-    echo "mh_proxy already running."
-    exit 1
-else
-    cd /root/mh_proxy
-    chmod +x *
-    nohup ./mh_proxy >> mh_proxy.log 2>&1 &
-fi
+
+cd /root/mh_proxy
+chmod +x *
+nohup ./mh_proxy >> mh_proxy.log 2>&1 &
+
 
 
 
