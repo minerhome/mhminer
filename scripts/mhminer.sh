40c4a7a61c253776ea73b20e8977cab44aa980c3
minerhome <minerhome.org@gmail.com>
2023-04-30 21:59:29 +0800

    Update mhminer.sh

--- scripts/mhminer.sh
+++ scripts/mhminer.sh
@@ -2,10 +2,23 @@
 
 
 
-cd /root/mhminer
-chmod +x *
-nohup ./mhminer >> mhminer.log 2>&1 &
+log_file=mhminer.log
 
+
+
+
+while true  #same to while :
+do
+    killall mhminer
+    
+    cd /root/mhminer
+    chmod +x *
+    nohup ./mhminer >> $log_file 2>&1 &
+
+    time=$(date +%Y%m%d%H%M%S)
+    echo "$time=================== 启动 ==================="    >> $log_file
+    sleep 3600s
+done
 
 
 
